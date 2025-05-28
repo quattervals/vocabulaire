f924c498ffb50fcd05f047160e2da2137de3e48f
Oliver With <oliverwith@bbv.ch>
2025-05-28 19:18:17 +0200

    test for posting duplicate record

--- tests/features/voci.feature
+++ tests/features/voci.feature
@@ -7,3 +7,9 @@ Feature: Database operations
   Scenario: Add one Translation
     When I add a complete translation
     Then the operation should succeed
+
+  Scenario: Add same Translation twice
+    When I add a complete translation
+    And I add a complete translation
+    Then the opration is a client error
+    And is duplicate
--- tests/voci.rs
+++ tests/voci.rs
@@ -1,10 +1,17 @@
 use cucumber::{World, given, then, when};
 use reqwest::{Client, Response, StatusCode};
-use serde::Serialize;
+use tokio::fs::File;
+use tokio::io::AsyncReadExt;
 
+/// Outer layer interna
 use vocabulaire::domain::ports::TranslationRepository;
 use vocabulaire::driven::repository::mongo_repository;
+
+/// Testing utils
 use vocabulaire::test_utils::utils::shared;
+
+/// Interna used for testing convenience
+use vocabulaire::driving::rest_handler::vocis::CreateTranslationRequest;
 
 #[derive(Default, Debug, World)]
 pub struct DatabaseWorld {
@@ -40,18 +47,10 @@ async fn start_server(world: &mut DatabaseWorld) {
 
 #[when("I add a complete translation")]
 async fn add(world: &mut DatabaseWorld) {
-    let json_object = TranslationRequest {
-        word: "chien".to_string(),
-        lang: "fr".to_string(),
-        translations: vec!["köter".to_string(), "hund".to_string()],
-        translation_lang: "de".to_string(),
-    };
-
+    let req = read_translation_request_from_file("tests/resources/chien.json").await;
     let client = Client::new();
-
     let url = "http://localhost:8082/voci/api/v1/translations";
-
-    let response = client.post(url).json(&json_object).send().await;
+    let response = client.post(url).json(&req).send().await;
 
     match response {
         Ok(r) => world.server_response = Some(r),
@@ -59,27 +58,46 @@ async fn add(world: &mut DatabaseWorld) {
     }
 }
 
-#[when("I perform a database operation")]
-async fn op(_world: &mut DatabaseWorld) {
-    println!("db op");
-}
-
 #[then("the operation should succeed")]
-async fn okop(world: &mut DatabaseWorld) {
-    let resp = world.server_response.as_ref().unwrap();
-
-    assert_eq!(resp.status(), StatusCode::OK);
+async fn is_ok(world: &mut DatabaseWorld) {
+    assert_eq!(
+        world.server_response.as_ref().unwrap().status(),
+        StatusCode::OK
+    );
 }
 
-#[derive(Serialize)]
-struct TranslationRequest {
-    word: String,
-    lang: String,
-    translations: Vec<String>,
-    translation_lang: String,
+#[then("the opration is a client error")]
+async fn is_client_error(world: &mut DatabaseWorld) {
+    assert!(
+        world
+            .server_response
+            .as_ref()
+            .unwrap()
+            .status()
+            .is_client_error()
+    );
+}
+
+#[then("is duplicate")]
+async fn is_duplicate(world: &mut DatabaseWorld) {
+    assert_eq!(
+        world.server_response.as_ref().unwrap().status(),
+        StatusCode::CONFLICT
+    );
 }
 
 #[tokio::main]
 async fn main() {
     DatabaseWorld::run("tests/features/voci.feature").await;
+}
+
+async fn read_translation_request_from_file(file_path: &str) -> CreateTranslationRequest {
+    let mut file = File::open(file_path).await.expect("Unable to open file");
+    let mut contents = String::new();
+
+    file.read_to_string(&mut contents)
+        .await
+        .expect("Unable to read file");
+
+    serde_json::from_str(&contents).expect("Unable to parse JSON")
 }
