28352397626dd7a87572fea0aef5835a65ef8403
Oliver With <oliverwith@bbv.ch>
2025-05-28 12:11:55 +0200

    cc work

--- tests/example.rs
+++ tests/example.rs
@@ -1,4 +1,4 @@
-use cucumber::{World, given, when, then};
+use cucumber::{World, given, then, when};
 
 // These `Cat` definitions would normally be inside your project's code,
 // not test code, but we create them here for the show case.
@@ -20,20 +20,15 @@ pub struct AnimalWorld {
     cat: Cat,
 }
 
-
-
-
-
 // #[given(regex = r"^a (hungry|satiated) cat$")]
 #[given(expr = "a {word} cat")]
 fn hungry_cat(world: &mut AnimalWorld, state: String) {
     match state.as_str() {
-        "hungry" =>  world.cat.hungry = true,
-        "satiated" =>  world.cat.hungry = false,
+        "hungry" => world.cat.hungry = true,
+        "satiated" => world.cat.hungry = false,
         _ => unreachable!(),
     }
 }
-
 
 #[when("I feed the cat")]
 fn feed_cat(world: &mut AnimalWorld) {
@@ -45,10 +40,31 @@ fn cat_is_fed(world: &mut AnimalWorld) {
     assert!(!world.cat.hungry);
 }
 
+#[cfg(test)]
+use vocabulaire::test_utils::utils::shared;
+
+use vocabulaire::driven::repository::mongo_repository;
+
+#[derive(Default, World)]
+pub struct DatabaseWorld {
+    repo: Option<mongo_repository::VociMongoRepository>,
+}
+// impl Default for DatabaseWorld {
+//     fn default() -> Self {
+//         Self { repo: None }
+//     }
+// }
+
+#[given("a clean database is available")]
+async fn setup_database(world: &mut DatabaseWorld) {
+    world.repo = Some(shared::setup_repo().await);
+}
+
 // This runs before everything else, so you can set up things here.
 fn main() {
     // You may choose any executor you like (`tokio`, `async-std`, etc.).
     // You may even have an `async` main, it doesn't matter. The point is that
     // Cucumber is composable. :)
     futures::executor::block_on(AnimalWorld::run("tests/features/simple.feature"));
+    futures::executor::block_on(AnimalWorld::run("tests/features/db.feature"));
 }
