c9da3a0539bfd62f2a550ddf8530ce06deefb76f
Oliver With <oliverwith@bbv.ch>
2025-05-28 12:49:19 +0200

    restructure main

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,12 @@
-use actix_web::dev::Server;
-use actix_web::middleware::Logger;
-use actix_web::{App, HttpServer, web, web::Data};
-
 use env_logger::Env;
 use vocabulaire::config::parse_local_config;
-use vocabulaire::domain::ports::TranslationRepository;
-use vocabulaire::driven::repository::mongo_repository::VociMongoRepository;
-use vocabulaire::driving::rest_handler;
+use vocabulaire::server;
 
 #[actix_web::main]
 async fn main() {
     env_logger::init_from_env(Env::new().filter_or("VOCI_LOG", "debug"));
-
     let config = parse_local_config();
-    let repo = VociMongoRepository::new(&config.persistence).unwrap();
-
-    create_server(repo)
-        .await
-        .unwrap()
-        .await
-        .expect("An error occurred while starting the web application");
-}
-
-async fn create_server(repo: impl TranslationRepository) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(move || {
-        App::new()
-            .wrap(Logger::default())
-            .app_data(Data::new(repo.clone()))
-            .configure(routes)
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run();
-    Ok(server)
-}
-
-fn routes(cfg: &mut web::ServiceConfig) {
-    cfg.service(
-        web::scope("/voci").service(
-            web::scope("/api/v1")
-                .service(
-                    web::resource("translations")
-                        .route(
-                            web::get()
-                                .to(rest_handler::vocis::read_translation::<VociMongoRepository>),
-                        )
-                        .route(
-                            web::post()
-                                .to(rest_handler::vocis::create_translation::<VociMongoRepository>),
-                        )
-                        .route(
-                            web::delete()
-                                .to(rest_handler::vocis::delete_translation::<VociMongoRepository>),
-                        )
-                        .route(
-                            web::put()
-                                .to(rest_handler::vocis::update_translation::<VociMongoRepository>),
-                        ),
-                )
-                .service(web::resource("translations/{id}")),
-        ),
-    );
+    if let Err(e) = server::start_server(config).await {
+        eprintln!("Failed to start server: {}", e);
+    }
 }
